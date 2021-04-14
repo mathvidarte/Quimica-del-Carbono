18d69045033a48aa341ed8f5eb83e247c8812c8c
Sok4h <55671660+Sok4h@users.noreply.github.com>
2021-04-14 13:02:01 -0500

    Elementos graficos juego

---
+++ sketch.js
@@ -0,0 +1,24 @@
+let bg;
+let puntos="Puntaje: 10"
+let myFont;
+let tiempo= 30;
+
+
+function preload(){
+
+    bg=loadImage("./data/PantalladeJuego.jpg");
+     myFont = loadFont('data/Jost-Bold.ttf');
+
+}
+function setup(){
+
+    createCanvas(1280,720);
+    background(bg);
+    textFont(myFont);
+    textSize(25);
+    fill('#4639B9');
+    text(puntos,637,90);
+
+    fill('#F1F1F1');
+    text(tiempo,550,90);
+}
